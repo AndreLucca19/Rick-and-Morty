655a41e7e9ce77a7ee69de27658d39e2ff412c12
André Lucca <157771933+AndreLucca19@users.noreply.github.com>
2025-04-10 11:39:28 -0300

    update home

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,17 @@ import "../styles/globals.css";
 
 			export const metadata = {
 				title: "My First NextJS App",
+        
 			};
-      
+
 			export default function RootLayout({ children }) {
 				return (
 					<html>
+            <head>
+            <link rel="icon" href="/favicon.png" />
+            </head>
 						<body>{children}</body>
 					</html>
+          
 );
 		}
